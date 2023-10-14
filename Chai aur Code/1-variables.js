630ec505b504ab85bce1c3d6610259e8a541ff45
ganeshbagav <ganeshbagav2001@gmail.com>
2023-10-14 14:25:20 +0530

    first commit

---
+++ Chai aur Code/1-variables.js
@@ -0,0 +1,7 @@
+const name = 'ganesh'
+var lastname = 'bagav' // variables created by var dont have scope
+let number = 123132  // let is introduced for recover problem of var 
+
+city = 'pune' 
+let address 
+console.table([name , lastname , number , city , address])
---
+++ Chai aur Code/2-dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as newer version
+let a=10
+let b= 'ghasg'
+let c =2.3
+let d = true
+
+// number
+// bigint
+// string
+// boolean
+// object
+// undefined
+// symbol
+
+console.log(null);
+console.log(undefined);
+
+console.log(typeof c);
